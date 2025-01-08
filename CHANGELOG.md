443ac57341fa9a4b2986e0b1e17fd831fc134002
mjayadharan <manu.jayadharan@gmail.com>
2025-01-08 15:12:30 -0600

    new release changes

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+
+## [v0.2.1] - 2025-01-08
+### Added
+- Updated readme.md to provide more information
+
+## [v0.2.0] - 2025-01-08
+### Added
+- New release testing
+- Updated readme.md to provide more information
+
+### Fixed
+- NIL
+
+## [0.1.0] - 2025-01-08
+### Added
+- Initial release
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DaeFinder",
-    version="0.1.0",
+    version="v0.2.1",
     description="A Python package to discover Differential Algebraic Equations from data.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
